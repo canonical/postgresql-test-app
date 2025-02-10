f86b20d4adb095ead653702c3e84a28af5750c02
Carl Csaposs <carl.csaposs@canonical.com>
2025-02-10 14:38:03 +0100

    Use `charmcraft test` & concierge

---
+++ tests/integration/conftest.py
@@ -0,0 +1,18 @@
+import subprocess
+
+import pytest
+
+from . import architecture
+
+
+@pytest.fixture(params=["lxd", "microk8s"], autouse=True)
+def cloud(cloud_name):
+    subprocess.run(["juju", "switch", cloud_name], check=True)
+
+
+@pytest.fixture
+def charm():
+    # Return str instead of pathlib.Path since python-libjuju's model.deploy(), juju deploy, and
+    # juju bundle files expect local charms to begin with `./` or `/` to distinguish them from
+    # Charmhub charms.
+    return f"./postgresql-test-app_ubuntu@22.04-{architecture.architecture}.charm"
